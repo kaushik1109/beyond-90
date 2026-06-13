32a4af739434217743d3a6e736e6df2b1ad84416
kaushik1109 <kaushiksahasranaman@gmail.com>
2026-06-13 10:24:00 +0200

    fix: imports

--- app/api/auth.py
+++ app/api/auth.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.core.database import SessionLocal, engine, Base
+from app.schemas.auth import CreateUser, UserResponse
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -9,5 +10,5 @@ async def login_user():
     pass
 
 @router.post("/register")
-async def register_user():
+async def register_user(user: CreateUser):
     pass
