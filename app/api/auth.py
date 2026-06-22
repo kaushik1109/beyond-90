62497465d1881bfd66c10cfb31f6913fbeeff115
kaushik1109 <kaushiksahasranaman@gmail.com>
2026-06-22 11:56:51 +0200

    db: password hashing added

--- app/api/auth.py
+++ app/api/auth.py
@@ -4,8 +4,13 @@ from app.core.database import SessionLocal, engine, Base
 from app.schemas.auth import CreateUser, UserResponse
 from app.models.auth import User
 from datetime import datetime
+from passlib.context import CryptContext
 
 router = APIRouter(prefix="/auth", tags=["auth"])
+pwd_context = CryptContext(
+        schemes=["bcrypt"],
+        deprecated="auto"
+)
 
 def get_db():
     db = SessionLocal()
@@ -15,14 +20,31 @@ def get_db():
         db.close()
 
 @router.post("/login")
-async def login_user():
-    pass
+async def login_user(email_id: str, password: str, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.email==email_id).first()
+
+    if user is None:
+        raise HTTPException(
+            status_code=400,
+            detail="Email id invalid"
+        )
+    
+    if pwd_context.verify(password, user.password):
+        return {"message":"Login successful"}
+    else:
+        raise HTTPException(
+            status_code=400,
+            detail="Wrong Password"
+        )
+
 
 @router.post("/register")
 async def register_user(user: CreateUser, db: Session = Depends(get_db)):
+    hashed_password = pwd_context.hash(user.password)
+    print("HASHED PASSWORD:", hashed_password)
     new_user = User (name = user.name,
                 email = user.email, 
-                password = user.password,
+                password = hashed_password,
                 created_at = datetime.now(),
                 updated_at = datetime.now() )
     existing_user = db.query(User).filter(User.email == new_user.email).first()
@@ -38,6 +60,7 @@ async def register_user(user: CreateUser, db: Session = Depends(get_db)):
     db.refresh(new_user)
 
     return {"message":"User created successfully"}
+
 
 @router.get("/users", response_model = list[UserResponse])
 async def get_users(db: Session = Depends(get_db)):
